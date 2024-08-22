1e55362602cc120895e2d51a46c707f52971624d
amira <princessba2002@gmail.com>
2024-08-22 03:22:10 +0300

    driver side menu 1st edit

---
+++ src/components/DriverSideMenu/index.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Navigation } from "react-minimal-side-navigation";
+import "react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css";
+import { useNavigate } from "react-router-dom";
+
+const SideMenu = () => {
+  const navigate = useNavigate();
+  return (
+      <Navigation
+        activeItemId="/management/members"
+        onSelect={({ itemId }) => {
+          navigate(itemId);
+        }}
+        items={[
+          {
+            title: "Dashboard",
+            itemId: "/Dashboard",
+          },
+          {
+            title: "User Management",
+            itemId: "/UserManagement",
+            subNav: [
+              {
+                title: "Drivers",
+                itemId: "/DriversManage",
+              },
+              {
+                title: "Client",
+                itemId: "/UsersManage",
+              },
+              {
+                title: "Staff",
+                itemId: "/StaffManage",
+              },
+            ],
+          },
+          {
+            title: "Orders Section",
+            itemId: "/OrdersSection",
+
+            subNav: [
+              {
+                title: "Full Order Details",
+                itemId: "/FullOrderDetails",
+              },
+              {
+                title: "Active Order Details",
+                itemId: "/ActiveOrders",
+              },
+            ],
+          },
+        ]}
+      />
+  );
+}
+export default SideMenu;
