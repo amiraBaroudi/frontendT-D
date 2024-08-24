78dfb967fc12011db55736e62274189d1799ec1e
amira <princessba2002@gmail.com>
2024-08-25 01:50:39 +0300

    driver history order edit

--- src/Pages/DriverOrderHistory.jsx
+++ src/Pages/DriverOrderHistory.jsx
@@ -1,31 +1,11 @@
-import { Typography, Table } from "antd";
+import { Typography } from "antd";
 import DriverLayout from "../components/DriverLayout";
 
 const OrderHistory = () => {
     return (
         <DriverLayout>
       <Typography.Title level={4}> Order History </Typography.Title>     
-    <Table
-      className="table"
-      columns={[
-        {
-          title: "User ID",
-          dataIndex: "Id",
-        },
-        {
-          title: "User Name",
-          dataIndex: "Email",
-        },
-        {
-          title: "Password",
-          dataIndex: "password",
-        },
-        {
-          title: "Role",
-          dataIndex: "boolean",
-        },
-      ]}
-    ></Table>
+    
     </DriverLayout>
   );
 }
