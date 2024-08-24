89170c99fa81aecc02f40618fe89f3e829f98596
amira <princessba2002@gmail.com>
2024-08-25 01:58:07 +0300

    route edit

---
+++ src/Pages/DriverDashboard.jsx
@@ -0,0 +1,11 @@
+
+import DriverLayout from "../components/DriverLayout";
+
+const DriverDashboard = () => {
+    return (
+        <DriverLayout>
+      
+    </DriverLayout>
+  );
+}
+export default DriverDashboard;
